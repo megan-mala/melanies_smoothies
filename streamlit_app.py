6544d63f4d2f69ab1391568677fad4490c7cb0ce
megan-mala <megan@thefeys.com>
2025-11-01 18:21:02 -0500

    Fix API URL for smoothiefroot nutrition info
    Updated API URL for smoothiefroot response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,6 @@ if ingredients_list:
 
 #New section to display smoothiefroot nutrition information
 import requests
-smoothiefroot_response = requests.get("http://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
 #sf_df = st.dataframe(data=smoothiefroot_response.json(), user_container_width=True)
