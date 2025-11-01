352e465e5477abb04ac7e57f00d0a151cf52f357
megan-mala <megan@thefeys.com>
2025-11-01 18:33:27 -0500

    Integrate Smoothiefroot API for watermelon data
    Added requests import and API call to fetch watermelon data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 # Write directly to the app
 st.set_page_config(layout="wide")
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -31,6 +32,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
+        sf_df = st.dataframe(data=smoothiefroot_response.json())
 
     st.write(ingredients_string)
 
@@ -44,8 +47,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order +'!', icon="✅")
 
-#New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json())
+
+
